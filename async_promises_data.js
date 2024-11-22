6ae3622a6732b0aff8e5cdc146c9c22b0f80ad42
UsiMxi17 <usimxi711@gmail.com>
2024-11-22 12:23:50 +0200

    first push

---
+++ async_promises_data.js
@@ -0,0 +1,44 @@
+[
+    {
+        "userID":"",
+        "firstName":"Zenande" ,
+        "lastName":"Nocanda",
+        "email":"zenandenocanda16@gmail.com",
+        "registrationDate":"29 January 2024",
+    },
+    {
+        "userID":"",
+        "firstName":"Asiphe" ,
+        "lastName":"Dumzela",
+        "email":"asiphesonadumzela31@gmail.com",
+        "registrationDate":"02 February 2024",
+    },
+    {
+        "userID":"",
+        "firstName":"Siyavuya" ,
+        "lastName":"Khohlani",
+        "email":"Siyavuyakhohlani2@gmail.com",
+        "registrationDate":"17 February 2024",
+    },
+    {
+        "userID":"",
+        "firstName":"Sivuyile" ,
+        "lastName":"Mtwetwe",
+        "email":"sivuyilemtwetwe@gmail.com",
+        "registrationDate":"22 January 2024",
+    },
+    {
+        "userId":"U$iMxi711",
+        "firstName":"Usisipho" ,
+        "lastName":"Mxinwa",
+        "email":"usimxi711@gmail.com",
+        "registrationDate":"02 February 2024",
+    },
+    {
+        "userID":"",
+        "firstName":"Avela" ,
+        "lastName":"Nikani",
+        "email":"avelarnikani47@gmail.com",
+        "registrationDate":"28 January 2024",
+    }
+]
